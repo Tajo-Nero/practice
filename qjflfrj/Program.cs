ec92f31da4810fa7dede00cd8f9435fb5b5388b4
Tajo-Nero <tajo-ostrich@naver.com>
2024-12-18 11:39:39 +0900

    [Feat]둘째 기능추가
    대략 3시간 걸쳐서 기능 추가 갈김

--- qjflfrj/Program.cs
+++ qjflfrj/Program.cs
@@ -15,6 +15,9 @@ namespace qjflfrj
             Console.WriteLine("둘째 커밋");
             Console.WriteLine("기능 개발중");
             Console.WriteLine("현재 상당히 복잡한 작업을 한 가정");
+            Console.WriteLine("맘에 안들어서 더 작업함");
+            Console.WriteLine("대략 3시간");
+
         }
     }
 }
