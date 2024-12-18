9371c1ae92d2840dd23a153cda338023cffa0f1b
Tajo-Nero <tajo-ostrich@naver.com>
2024-12-18 11:38:13 +0900

    [Feat]기능 추가
    기능추가 설명~~~~~

--- qjflfrj/Program.cs
+++ qjflfrj/Program.cs
@@ -13,6 +13,8 @@ namespace qjflfrj
             Console.WriteLine("연습용");
             Console.WriteLine("커밋메시지를 상세하게 적을수없다");
             Console.WriteLine("둘째 커밋");
+            Console.WriteLine("기능 개발중");
+            Console.WriteLine("현재 상당히 복잡한 작업을 한 가정");
         }
     }
 }
